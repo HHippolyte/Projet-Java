321df38d980612401c1b1d51ed58138e82d64b93
Maxxxxx <36278928+Maximilien39@users.noreply.github.com>
2018-02-24 10:04:50 +0100

    Update Abonne.java

--- Abonne.java
+++ Abonne.java
@@ -12,13 +12,13 @@ package projetjava;
 public class Abonne extends Personne{
     private String loginAbonne;
     private String motDePasseAbonne;
-    private Reservation procede;
-    public Abonne (String nm, String pre,String tel, String em, String la, String mdpa, Reservation proc)
+ 
+    public Abonne (String nm, String pre,String tel, String em, String la, String mdpa)
     {
         super(nm,pre,tel,em);
         loginAbonne=la;
         motDePasseAbonne=mdpa;
-        procede=proc;
+        
     }
 
     public String getLoginAbonne() {
@@ -37,13 +37,6 @@ public class Abonne extends Personne{
         this.motDePasseAbonne = motDePasseAbonne;
     }
 
-    public void setProcede(Reservation procede) {
-        this.procede = procede;
-    }
-
-    public Reservation getProcede() {
-        return procede;
-    }
     
 }
     
